32e41cfbafd589da00296db6e7eb2f7df1fc3759
ANDRES H CARRERA <andreshcar@live.com>
2018-10-12 14:04:31 -0600

    adding the basics for the pentecontroller

---
+++ PenteGame.Lib/Controllers/PenteController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PenteGame.Lib.Controllers
+{
+    public class PenteController
+    {
+        public void TakeTurn()
+        {
+        }
+
+        public Boolean CheckVerticalCapture()
+        {
+            return false;
+        }
+        public Boolean CheckHorizontalCapture()
+        {
+            return false;
+        }
+        public Boolean CheckVerticalTessara()
+        {
+            return false;
+        }
+        public Boolean CheckHorizontalTessara()
+        {
+            return false;
+        }
+        public Boolean CheckVerticalTria()
+        {
+            return false;
+        }
+        public Boolean CheckHorizontalTria()
+        {
+            return false;
+        }
+        public Boolean CheckVerticalWin()
+        {
+            return false;
+        }
+        public Boolean CheckHorizontalWin()
+        {
+            return false;
+        }
+    }
+}
