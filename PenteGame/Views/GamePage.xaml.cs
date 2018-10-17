c9ee740ba8629626f5ab930076142231c4ee9f9d
Nick <cooltop101@gmail.com>
2018-10-17 14:42:07 -0600

    Stuff with the grid

--- PenteGame/Views/GamePage.xaml.cs
+++ PenteGame/Views/GamePage.xaml.cs
@@ -26,28 +26,89 @@ namespace PenteGame.Views
             InitializeComponent();
         }
 
-        private void FillGrid()
-        {
-            var colorBrush = new BrushConverter().ConvertFromString("#87A885") as SolidColorBrush;
 			ImageBrush gray = new ImageBrush(new BitmapImage(new Uri(@"./Resources/GreyPiece.png", UriKind.Relative)));
 			ImageBrush purple = new ImageBrush(new BitmapImage(new Uri(@"./Resources/PurplePiece.png", UriKind.Relative)));
-			for (int i = 0; i < 19*19; i++)
-            {
+			ImageBrush intersection =  new ImageBrush(new BitmapImage(new Uri(@"./Resources/Intersection.png", UriKind.Relative)));
+			ImageBrush topSide = new ImageBrush(new BitmapImage(new Uri(@"./Resources/Side.png", UriKind.Relative)));
+			ImageBrush leftSide = new ImageBrush(new BitmapImage(new Uri(@"./Resources/LeftSide.png", UriKind.Relative)));
+			ImageBrush rightSide = new ImageBrush(new BitmapImage(new Uri(@"./Resources/RightSide.png", UriKind.Relative)));
+			ImageBrush bottomSide = new ImageBrush(new BitmapImage(new Uri(@"./Resources/BottomSide.png", UriKind.Relative)));
+			ImageBrush upperLeft = new ImageBrush(new BitmapImage(new Uri(@"./Resources/Corner.png", UriKind.Relative)));
+			ImageBrush upperRight = new ImageBrush(new BitmapImage(new Uri(@"./Resources/UpperRight.png", UriKind.Relative)));
+			ImageBrush lowerLeft = new ImageBrush(new BitmapImage(new Uri(@"./Resources/LowerLeft.png", UriKind.Relative)));
+			ImageBrush lowerRight = new ImageBrush(new BitmapImage(new Uri(@"./Resources/LowerRight.png", UriKind.Relative)));
+		private void FillGrid()
+        {
+            var colorBrush = new BrushConverter().ConvertFromString("#87A885") as SolidColorBrush;
+			//Brush gray = new Brush();
+			for (int i = 0; i < 19; i++)
+			{
+				for (int j = 0; j < 19; j++)
+				{
+					Rectangle rect = new Rectangle
+					{
+						Fill = intersection
+					};
 
-				var rect = new Rectangle
-				{
-					Fill = gray,
-					Stroke = colorBrush
-				};
-				if (i%2==0)
-				{
-					rect.Fill = purple;
+					//Replaces fill with side image
+					if (i==0)
+					{
+						rect.Fill = topSide;
+					}
+					if (i==18)
+					{
+						rect.Fill = bottomSide;
+					}
+					if (j == 0)
+					{
+						rect.Fill = leftSide;
+					}
+					if (j == 18)
+					{
+						rect.Fill = rightSide;
+					}
+
+					//replaces corner with corner image
+					if (i == 0 && j == 0)
+					{
+						rect.Fill = upperLeft;
+					}
+					if (i == 0 && j == 18)
+					{
+						rect.Fill = upperRight;
+					}
+					if (i == 18 && j == 0)
+					{
+						rect.Fill = lowerLeft;
+					}
+					if (i == 18 && j == 18)
+					{
+						rect.Fill = lowerRight;
+					}
+
+
+
+					rect.MouseDown += (s, e) => AddPiece(s, e);
+					this.GameGrid.Children.Add(rect);
 				}
-				//rect.MouseDown += (s, e) => MessageBox.Show($"Bonjour {e.GetPosition(this.GameGrid)}");
-				rect.MouseDown += (s, e) => AddPiece(s,e);
-				Console.WriteLine("Added rect " + rect);
-				this.GameGrid.Children.Add(rect);
-            }
+			}
+			//for (int i = 0; i < 19*19; i++)
+   //         {
+
+			//	var rect = new Rectangle
+			//	{
+			//		Fill = intersection,
+			//		//Stroke = colorBrush
+			//	};
+
+			//	//if (i%2==0)
+			//	//{
+			//	//	rect.Fill = purple;
+			//	//}
+			//	//rect.MouseDown += (s, e) => MessageBox.Show($"Bonjour {e.GetPosition(this.GameGrid)}");
+			//	//rect.MouseDown += (s, e) => AddPiece(s,e);
+			//	//this.GameGrid.Children.Add(rect);
+   //         }
         }
 
         public event Action<PageRequest> PageChangeRequested;
@@ -59,24 +120,21 @@ namespace PenteGame.Views
 		bool pTurn = true;
 		private void AddPiece(object sender, MouseButtonEventArgs e)
 		{
-			Uri peice = new Uri("/Resources/PurplePiece.png", UriKind.Relative);
-			if (pTurn)
+
+			Rectangle rect = (Rectangle)sender;
+			if (!rect.Fill.Equals(gray) || !rect.Fill.Equals(purple))
 			{
-				peice = new Uri("/Resources/GreyPiece.png", UriKind.Relative);
-			}
-			string BodyName = "Piece";
-			Image BodyImage = new Image
-			{
-				Width = 20,
-				Height = 20,
-				Name = BodyName,
-				Source = new BitmapImage(peice)
-			};
-			Console.WriteLine("Add");
-			PieceGrid.Children.Add(BodyImage);
-			Canvas.SetTop(BodyImage, e.GetPosition(this.GameGrid).Y);
-			Canvas.SetLeft(BodyImage, e.GetPosition(this.GameGrid).X);
+				if (pTurn)
+				{
+					rect.Fill = gray;
+				}
+				else
+				{
+					rect.Fill = purple;
+				}
 			pTurn = !pTurn;
+
+			}
 		}
 	}
 }
