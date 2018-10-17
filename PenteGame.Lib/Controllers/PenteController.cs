b45f1e3191353bf2e9f41e51b536d70b0da39d18
ANDRES H CARRERA <andreshcar@live.com>
2018-10-17 14:42:38 -0600

    finalizing the captures for both the vertical and horizontal captures

--- PenteGame.Lib/Controllers/PenteController.cs
+++ PenteGame.Lib/Controllers/PenteController.cs
@@ -7,6 +7,8 @@ namespace PenteGame.Lib.Controllers
 {
     public class PenteController
     {
+        private const int MaxDistanceForCapture = 4;
+        private const int ClosingBracketDistance = 3;
         public event Action<PieceColor> Tessara;
         public event Action<PieceColor> Tria;
         public event Action<PieceColor> Win;
@@ -41,6 +43,14 @@ namespace PenteGame.Lib.Controllers
 
         public PieceColor CurrentTurn { get; set; }
         public IEnumerable<GamePiece> Pieces => _board.Values;
+
+        public PenteController()
+        {
+            _board = new Dictionary<Point, GamePiece>();
+            _captures = new Dictionary<PieceColor, int>();
+            //subscribe to our own capture event so we can increment our capture count;
+        }
+
         public int GetTotalCaptures(PieceColor color)
         {
             return _captures[color];
@@ -48,77 +58,126 @@ namespace PenteGame.Lib.Controllers
 
         public bool TakeTurn(Point placement, PieceColor color) //Returns bool result depending on the validity of the position
         {
+            bool isValidMove = false;
             if (_board.ContainsKey(placement) || color != CurrentTurn || !IsOnBoard(placement))
             {
-                return false;
+                isValidMove = true;
+                CoordinateMoves(placement, color);
             }
-            return true;
+            return isValidMove;
         }
 
-        private bool CheckVerticalCapture(Point placement, PieceColor color) //Check if a piece could be captured vertically and captures if true
+        private void CoordinateMoves(Point placement, PieceColor color)
         {
-            int i;
-            for (i = 1; i < 4; i++)
-            {
-                Point positionDown = new Point(placement.x, placement.y - i);
-                //TODO: Add logic for positionUp
-                if (DoesPieceExist(positionDown)) //Checks if pieces within a range of 4 exists
-                {
-                    if (i < 3 && color == _board[positionDown].Color)
-                    {
-                        break;
-                    }
-                    else if (i == 3 && color != _board[positionDown].Color)
-                    {
-                        break;
-                    }
-                }
-                else
-                {
-                    break;
-                }
-            }
-            if (i == 4)
-            {
-                Capture(color);
-                _board.Remove(new Point(placement.x, placement.y - 1));
-                _board.Remove(new Point(placement.x, placement.y - 2));
-                return true;
-            }
-            return false;
+            // first check if there is a capture
+            // then check if there is a tria
+            //if there is a tria check for tessera
+            //if there is no tessera fire tria event.
+            // if there is a tessera check for win
+            // if there is no win fire the tessera event.
+            //if there is a win fire the win event and hold it.
         }
 
-        private bool CheckHorizontalCapture()
+        private int CaptureMethod(Point basePoint, Func<Point, int, Point> getNextPoint)
         {
-            return false;
+            int distanceCounter = 0;
+            Point positionDown;
+            do
+            {
+                positionDown = getNextPoint(basePoint, distanceCounter);
+
+            } while (distanceCounter < MaxDistanceForCapture &&
+                    CheckIfPointExists(positionDown) &&
+                    (CheckIfOppositePiece(positionDown, _board[basePoint].Color, distanceCounter) ||
+                    CheckIfJoiningPiece(positionDown, _board[basePoint].Color, distanceCounter)));
+
+            return distanceCounter;
         }
+
+        private bool ProccessCapture(Point ogPiece, Func<Point, int, Point> calculateNextPoint)
+        {
+            bool captureHasHappened = false;
+            int totalFound = CaptureMethod(ogPiece, calculateNextPoint);
+            if (totalFound == MaxDistanceForCapture)
+            {
+                captureHasHappened = true;
+                _board.Remove(calculateNextPoint(ogPiece, 1));
+                _board.Remove(calculateNextPoint(ogPiece, 2));
+            }
+            return captureHasHappened;
+        }
+
+        private bool CheckVerticalCapture(Point placement) //Check if a piece could be captured vertically and captures if true
+        {
+            bool hasBeenAVerticalCapture = ProccessCapture(placement, (point, mod) => point.AddToY(mod));
+            hasBeenAVerticalCapture = ProccessCapture(placement, (point, mod) => point.AddToY(-mod)) || hasBeenAVerticalCapture;
+            return hasBeenAVerticalCapture;
+        }
+
+        private bool CheckHorizontalCapture(Point newestPiece)
+        {
+            bool horizontalCaptureHasBeenFound = ProccessCapture(newestPiece, (point, mod) => point.AddToX(mod));
+            horizontalCaptureHasBeenFound = ProccessCapture(newestPiece, (point, mod) => point.AddToX(-mod)) || horizontalCaptureHasBeenFound;
+            return horizontalCaptureHasBeenFound;
+        }
+
+        //some method to check recursively???
+        //how would that work??
+        //when do we break??
+        //if break in pattern???
+        // what would be the pattern???
+        // we could for the tessera, tria, and win
+        // we need to account for when a center piece is placed for the win
+        // connect four logic
+        // if non-existent || color != ours break out of recursion maybe??
+        // non-existent meaning either a null object at point or point not even on board.
+        // then check in the opposite but same direction??
+        // so in a sense we check down then go up
+        // if we checked left then go right
+        // if we checked diagonally upper left check downward right
+        // if we checked diagonally upper right check downward left
 
         private bool CheckVerticalTessara()
         {
-            return false;
+            bool verticalTesseraIsFound = false;
+            //check if there is a tessera down
+            //check if there is a tessera up
+            return verticalTesseraIsFound;
         }
 
         private bool CheckHorizontalTessara()
         {
-            return false;
+            bool horizontalTesseraIsFound = false;
+            //check if there is a tessera left 
+            //check if there is a tessera right.
+            return horizontalTesseraIsFound;
         }
 
         private bool CheckVerticalTria()
         {
-            return false;
+            bool verticalTriaHasBeenFound = false;
+            //check for tria and yeah
+            return verticalTriaHasBeenFound;
         }
 
         private bool CheckHorizontalTria()
         {
-            return false;
+            bool horizontalTriaHasBeenFound = false;
+            return horizontalTriaHasBeenFound;
         }
         private bool CheckVerticalWin()
         {
-            return false;
+            bool verticalWinHasHappened = false;
+            return verticalWinHasHappened;
         }
+
+        //maybe we don't need to 
+        //check one of the wins after first true?
+
         private bool CheckHorizontalWin()
         {
-            return false;
+            bool horizontalWinHasHappened = false;
+            return horizontalWinHasHappened;
         }
 
         private bool IsOnBoard(Point placement)
@@ -126,9 +185,21 @@ namespace PenteGame.Lib.Controllers
             return ((placement.x >= 0 && placement.x <= Width) && (placement.y >= 0 && placement.y <= Height));
         }
 
-        private bool DoesPieceExist(Point placement)
+        private bool CheckIfPointExists(Point placement)
         {
             return IsOnBoard(placement) && _board.ContainsKey(placement);
         }
+
+        private bool CheckIfOppositePiece(Point positionDown, PieceColor color, int distanceCounter)
+        {
+            //checks if the distance is less than the edge of the bracket and makes sure that its color is not the same.
+            return (distanceCounter < ClosingBracketDistance && _board[positionDown].Color != color);
+        }
+        private bool CheckIfJoiningPiece(Point positionDown, PieceColor color, int distanceCounter)
+        {
+            // checks if it is the end point for a bracket and then if that point has the same color as the one passed in.
+            return (distanceCounter == ClosingBracketDistance && _board[positionDown].Color == color);
+        }
+
     }
 }
--- PenteGame/Views/GamePage.xaml.cs
+++ PenteGame/Views/GamePage.xaml.cs
@@ -1,17 +1,10 @@
 ﻿using PenteGame.Views.Intefaces;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace PenteGame.Views
@@ -29,24 +22,24 @@ namespace PenteGame.Views
         private void FillGrid()
         {
             var colorBrush = new BrushConverter().ConvertFromString("#87A885") as SolidColorBrush;
-			ImageBrush gray = new ImageBrush(new BitmapImage(new Uri(@"./Resources/GreyPiece.png", UriKind.Relative)));
-			ImageBrush purple = new ImageBrush(new BitmapImage(new Uri(@"./Resources/PurplePiece.png", UriKind.Relative)));
-			for (int i = 0; i < 19*19; i++)
+            ImageBrush gray = new ImageBrush(new BitmapImage(new Uri(@"./Resources/GreyPiece.png", UriKind.Relative)));
+            ImageBrush purple = new ImageBrush(new BitmapImage(new Uri(@"./Resources/PurplePiece.png", UriKind.Relative)));
+            for (int i = 0; i < 19 * 19; i++)
             {
 
-				var rect = new Rectangle
-				{
-					Fill = gray,
-					Stroke = colorBrush
-				};
-				if (i%2==0)
-				{
-					rect.Fill = purple;
-				}
-				//rect.MouseDown += (s, e) => MessageBox.Show($"Bonjour {e.GetPosition(this.GameGrid)}");
-				rect.MouseDown += (s, e) => AddPiece(s,e);
-				Console.WriteLine("Added rect " + rect);
-				this.GameGrid.Children.Add(rect);
+                var rect = new Rectangle
+                {
+                    Fill = gray,
+                    Stroke = colorBrush
+                };
+                if (i % 2 == 0)
+                {
+                    rect.Fill = purple;
+                }
+                //rect.MouseDown += (s, e) => MessageBox.Show($"Bonjour {e.GetPosition(this.GameGrid)}");
+                rect.MouseDown += (s, e) => AddPiece(s, e);
+                Console.WriteLine("Added rect " + rect);
+                GameGrid.Children.Add(rect);
             }
         }
 
@@ -56,27 +49,28 @@ namespace PenteGame.Views
         {
             FillGrid();
         }
-		bool pTurn = true;
-		private void AddPiece(object sender, MouseButtonEventArgs e)
-		{
-			Uri peice = new Uri("/Resources/PurplePiece.png", UriKind.Relative);
-			if (pTurn)
-			{
-				peice = new Uri("/Resources/GreyPiece.png", UriKind.Relative);
-			}
-			string BodyName = "Piece";
-			Image BodyImage = new Image
-			{
-				Width = 20,
-				Height = 20,
-				Name = BodyName,
-				Source = new BitmapImage(peice)
-			};
-			Console.WriteLine("Add");
-			PieceGrid.Children.Add(BodyImage);
-			Canvas.SetTop(BodyImage, e.GetPosition(this.GameGrid).Y);
-			Canvas.SetLeft(BodyImage, e.GetPosition(this.GameGrid).X);
-			pTurn = !pTurn;
-		}
-	}
+
+        private bool pTurn = true;
+        private void AddPiece(object sender, MouseButtonEventArgs e)
+        {
+            Uri peice = new Uri("/Resources/PurplePiece.png", UriKind.Relative);
+            if (pTurn)
+            {
+                peice = new Uri("/Resources/GreyPiece.png", UriKind.Relative);
+            }
+            string BodyName = "Piece";
+            Image BodyImage = new Image
+            {
+                Width = 20,
+                Height = 20,
+                Name = BodyName,
+                Source = new BitmapImage(peice)
+            };
+            Console.WriteLine("Add");
+            PieceGrid.Children.Add(BodyImage);
+            Canvas.SetTop(BodyImage, e.GetPosition(GameGrid).Y);
+            Canvas.SetLeft(BodyImage, e.GetPosition(GameGrid).X);
+            pTurn = !pTurn;
+        }
+    }
 }
